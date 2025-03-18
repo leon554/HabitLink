f6896079ca54675a40b3c2e649cc3a9b51791cfc
Leon <codetips4572@gmail.com>
2025-03-18 17:07:10 +1100

    issue

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const userRoutes = require("./routes/users")
 const cors = require("cors");
 
 const app = express()
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json())
 mongoose.connect(process.env.MONGO_URI)
 app.use("/auth", authRoutes)
