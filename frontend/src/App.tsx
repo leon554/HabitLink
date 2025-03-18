2dcbb4503cbb685ab620312d02c38639f1ccee69
Leon <codetips4572@gmail.com>
2025-03-18 16:57:39 +1100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
               <Route path="/log" element={<Log/>}/>
             </Route>
           </Routes>
-        </Router>
+        </Router> 
       </UserProvider>
     </>
   ); 
